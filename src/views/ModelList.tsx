48394668502b1420b3fb9e2e60f3941c6b6dc07a
Louis Wilke <louiswilke@gmx.de>
2022-09-01 20:12:08 +0200

    able to view models in map browser

--- src/views/ModelList.tsx
+++ src/views/ModelList.tsx
@@ -14,7 +14,11 @@ import { Vector3 } from "three";
 declare type ModelObject = {
     modelPath: string;
     hasTexture?: boolean;
-}
+};
+
+declare type SelectedModelObject = ModelObject & {
+    isMapModel: boolean;
+};
 
 const apiManager = ApiManager.instance;
 const storageManager = StorageManager.instance;
@@ -47,15 +51,17 @@ function folderToTreeNodes( dir: ModelFolderObject, icon?: (node: TreeNodeInfo<M
     return nodes;
 }
 
-let _mapNodes = [] as TreeNodeInfo<ModelObject>[];
-let _browserNodes = [] as TreeNodeInfo<ModelObject>[];
+let _mapNodes : TreeNodeInfo<ModelObject>[]|undefined;
+let _browserNodes : TreeNodeInfo<ModelObject>[]|undefined;
+let loadingMapModels = false;
+let loadingBrowserModels = false;
 
 export default function ModelList() {
     
     const [loading, setLoading] = useState(false);
-    const [mapNodes, setMapNodes] = useState([] as TreeNodeInfo<ModelObject>[]);
-    const [browserNodes, setBrowserNodes] = useState([] as TreeNodeInfo<ModelObject>[]);
-    const [model, setModel] = useState<ModelObject>();
+    const [mapNodes, setMapNodes] = useState<TreeNodeInfo<ModelObject>[]>();
+    const [browserNodes, setBrowserNodes] = useState<TreeNodeInfo<ModelObject>[]>();
+    const [model, setModel] = useState<SelectedModelObject>();
     const navigation = useNavigate();
 
     _mapNodes = mapNodes;
@@ -71,16 +77,19 @@ export default function ModelList() {
         setLoading(true);
         storageManager.updateAppState("modelDirectory", apiManager.modelDirectory);
         try {
+            loadingBrowserModels = true;
+            
             setBrowserNodes(
                 folderToTreeNodes(
                     await apiManager.getModels(apiManager.modelDirectory),
-                    node => typeof node.nodeData?.hasTexture === "boolean" ? <Icon icon="cube-add" intent="success" onClick={() => addModelToMap(node)} /> : undefined
+                    node => typeof node.nodeData!.hasTexture === "boolean" ? <Icon icon="cube-add" intent="success" onClick={() => addModelToMap(node)} /> : undefined
                 )
             );
             showMessage("Local models loaded", "success");
         } catch(e) {
             handleError(e);
         }
+        loadingBrowserModels = false;
         setLoading(false);
     }
 
@@ -89,19 +98,24 @@ export default function ModelList() {
      */
     async function loadMapModels() {
         if (mapName === undefined) return;
+        loadingMapModels = true;
         try {
-            setMapNodes(folderToTreeNodes(await apiManager.getMapModels(mapName), node => <Icon icon="trash" intent="danger" onClick={() => removeModelFromMap(node)} />));
+            setMapNodes(folderToTreeNodes(await apiManager.getMapModels(mapName), node => <Icon icon="trash" intent="danger" onClick={(e) => { e.stopPropagation(); removeModelFromMap(node); return false; }} />));
             showMessage("Map models loaded", "success");
         } catch(e) {
             handleError(e);
         }
+        loadingMapModels = false;
     }
 
     // on first component mount: load models if the path is given
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
-        if (apiManager.modelDirectory !== undefined) loadAllModels().catch(handleError);
-        if (apiManager.modelDirectory !== undefined) loadMapModels().catch(handleError);
+        if (apiManager.modelDirectory === undefined) return;
+        Promise.all([
+            !loadingBrowserModels ? loadAllModels() : Promise.resolve(),
+            !loadingMapModels ? loadMapModels() : Promise.resolve()
+        ]).catch(handleError);
     }, []);
 
     /**
@@ -110,6 +124,8 @@ export default function ModelList() {
      */
     function mapNodeClicked( node: TreeNodeInfo<ModelObject> ) {
         if (node.nodeData === undefined || mapName === undefined) return;
+        console.log(`Showing 3D model "${node.id}"`);
+        setModel(Object.assign({ isMapModel: true }, node.nodeData) as SelectedModelObject);
     }
 
     /**
@@ -119,7 +135,7 @@ export default function ModelList() {
     function removeModelFromMap( node: TreeNodeInfo<ModelObject> ) {
         if (node.nodeData === undefined || mapName === undefined) return;
         apiManager.removeModelFromMap(mapName, node.nodeData.modelPath)
-            .then(() => setMapNodes(_mapNodes.filter( n => n.id !== node.id )))
+            .then(() => setMapNodes((_mapNodes||[]).filter( n => n.id !== node.id )))
             .catch(handleError);
     }
 
@@ -132,10 +148,10 @@ export default function ModelList() {
         if (node.nodeData.hasTexture === undefined) {
             node.isExpanded = !node.isExpanded;
             node.icon = node.isExpanded ? "folder-open" : "folder-close";
-            setBrowserNodes(browserNodes.concat());
+            setBrowserNodes((_browserNodes||[]).concat());
         } else {
             console.log(`Showing 3D model "${node.id}"`);
-            setModel(node.nodeData);
+            setModel(Object.assign({ isMapModel: false }, node.nodeData) as SelectedModelObject);
         }
     }
 
@@ -144,15 +160,18 @@ export default function ModelList() {
      * @param node The clicked node (3D model or folder)
      */
     function addModelToMap( node: TreeNodeInfo<ModelObject> ) {
+        console.log(node);
         if (mapName === undefined || node.nodeData === undefined || node.nodeData.hasTexture === undefined) return;
-        if (_mapNodes.findIndex(n => n.id === node.id) !== -1) {
+        if ((_mapNodes||[]).findIndex(n => n.id === node.id) !== -1) {
             showMessage(`Already added "${node.id}" to "${mapName}"...`, "warning");
             return;
         }
         const _node = Object.assign({}, node);
-        _node.secondaryLabel = <Icon icon="minus" onClick={() => removeModelFromMap(_node)} />
+        _node.nodeData = Object.assign({}, node.nodeData);
+        _node.nodeData!.modelPath = node.nodeData!.modelPath.substring(node.nodeData!.modelPath.lastIndexOf(":")+1);
+        _node.secondaryLabel = <Icon icon="trash" intent="danger" onClick={(e) => { e.stopPropagation(); removeModelFromMap(_node); return false; }} />
         apiManager.addModelToMap(mapName, node.nodeData.modelPath, node.nodeData.hasTexture ? node.nodeData.modelPath : undefined)
-            .then(() => setMapNodes(_mapNodes.concat([_node])))
+            .then(() => setMapNodes((_mapNodes||[]).concat([_node])))
             .catch(handleError);
     }
 
@@ -168,7 +187,12 @@ export default function ModelList() {
         if (storageManager.getAppState("modelDirectory") === undefined) {
             showError("Must select a model directory first!");
         } else {
-            modelNode = <Model modelUrl={apiManager.getModelUrl("mesh", model.modelPath)} textureUrl={model.hasTexture ? apiManager.getModelUrl("texture", model.modelPath) : undefined} />;
+            modelNode = (
+                <Model
+                    modelUrl={apiManager.getModelUrl("mesh", model.modelPath, model.isMapModel ? mapName : undefined)}
+                    textureUrl={model.hasTexture ? apiManager.getModelUrl("texture", model.modelPath, model.isMapModel ? mapName : undefined) : undefined}
+                />
+            );
         }
     }
 
@@ -176,7 +200,7 @@ export default function ModelList() {
         <>
             <Page
                 title="Model Browser"
-                style={{ display: "flex", flexDirection: "row", flex: 1 }}
+                style={{ display: "flex", flexDirection: "row", flex: 1, gap: "2em", padding: "2em" }}
                 headerComponents={
                     <>
                         <InputGroup
@@ -190,30 +214,31 @@ export default function ModelList() {
                     </>
                 }
             >
-                <Card style={{ minWidth: "30em", margin: "1em", padding: 0, display: "flex", flexDirection: "column" }} elevation={Elevation.THREE} >
+                <Card style={{ minWidth: "30em", padding: 0, display: "flex", flexDirection: "column" }} elevation={Elevation.THREE} >
                     <h2 style={{ textAlign: "center" }}>{mapName}</h2>
-                    <div style={{ overflowY: "scroll", flex: 1 }}>
+                    <div style={{ overflowY: "scroll", flex: 1, borderTop: "1px solid lightgrey" }}>
                         <Tree
-                            contents={mapNodes}
+                            contents={mapNodes||[]}
                             onNodeExpand={mapNodeClicked}
                             onNodeCollapse={mapNodeClicked}
                             onNodeClick={mapNodeClicked}
                         />
                     </div>
                 </Card>
-                <Card style={{ minWidth: "30em", margin: "1em", padding: 0, display: "flex", flexDirection: "column" }} elevation={Elevation.THREE} >
+                <Card style={{ minWidth: "30em", padding: 0, display: "flex", flexDirection: "column" }} elevation={Elevation.THREE} >
                     <h2 style={{ textAlign: "center" }}>Model Browser</h2>
-                    <div style={{ overflowY: "scroll", flex: 1 }}>
+                    <div style={{ overflowY: "scroll", flex: 1, borderTop: "1px solid lightgrey" }}>
                         <Tree
-                            contents={browserNodes}
+                            contents={browserNodes||[]}
                             onNodeExpand={browserNodeClicked}
                             onNodeCollapse={browserNodeClicked}
                             onNodeClick={browserNodeClicked}
                         />
                     </div>
                 </Card>
-                <Card style={{ flex: 3, margin: "1em", padding: 0 }} elevation={Elevation.THREE}>
-                    <Canvas>
+                <Card style={{ flex: 3, padding: 0 }} elevation={Elevation.THREE}>
+                    <h2 style={{ textAlign: "center" }}>{model ? model.modelPath.substring(model.modelPath.lastIndexOf(":")+1) : "Select a model..."}</h2>
+                    <Canvas style={{ borderTop: "1px solid lightgrey" }}>
                         <CameraController />
                         <pointLight position={new Vector3(25, 25, 25)} intensity={0.6} />
                         <pointLight position={new Vector3(-25, -25, -25)} intensity={0.6} />
